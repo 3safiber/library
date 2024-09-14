bb1ae24dfba501ab46e3f49573bfa6df05aa1f49
abdallah salameh <salameha595@gmail.com>
2024-09-14 10:27:53 +0300

    test bookXML file

--- models/book.py
+++ models/book.py
@@ -2,6 +2,14 @@ from odoo import models, fields
 
 class MyModel(models.Model):
     _name = 'book.model'
+    
     _description = 'My book model'
-
+    
     name = fields.Char(string='Name')
+    
+    # states = fields.Selection(
+    #     string='state',
+    #     selection=[('draft', 'Draft'), ('done', 'Done')]
+    # )
+    
+    
