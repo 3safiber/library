36b53429b874f1a5e4ab7da67cf1418fedcfe4e5
abdallah salameh <salameha595@gmail.com>
2024-09-15 10:27:03 +0300

    check book by

--- models/book.py
+++ models/book.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from odoo.exceptions import ValidationError
 
 class MyModel(models.Model):
 
@@ -6,11 +7,12 @@ class MyModel(models.Model):
     _description = 'My book model'
     name = fields.Char(string='Name')
     test = fields.Char(string='Test')
-    states = fields.Selection(
+    state = fields.Selection(
         string='state',
-        selection=[('draft', 'Draft'), ('done', 'Done'),
-        ('test', 'Test'),('test2', 'Test2')
-        ]
+        selection=[('draft', 'Draft'), ('bending', 'Bending'),
+        ('sold', 'Sold')
+        ],
+        default='draft'
     )
     
     description = fields.Text()
@@ -18,8 +20,44 @@ class MyModel(models.Model):
     date_availability = fields.Date()
     expected_price = fields.Float()
     selling_price = fields.Float()
+    
+    
+    diff = fields.Float(compute='_compute_diff')
+    
+    increment = fields.Integer()
+    
     bedrooms =fields.Integer()
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden_area = fields.Boolean()
+    
+    @api.depends('expected_price', 'selling_price')
+    def _compute_diff(self):
+      for record in self:
+        record.diff = record.expected_price - record.selling_price
+            
+    @api.onchange('description')
+    def _onchange_description(self):
+      self.increment +=1
+                            
+    @api.constrains('bedrooms')
+    def _check_bedrooms_valid_number(self):
+      for record in self:
+        if record.bedrooms < 0 or record.bedrooms >= 30:
+          raise ValidationError('please enter a valid number between 0 and 30')    
+    
+    def action_draft(self):
+      for rec in self:
+        rec.state = 'draft'
+        # rec.write({
+        #   'state':'draft'
+        # })
+        
+    def action_bending(self):
+      for rec in self:
+        rec.state = 'bending'
+        
+    def action_sold(self):
+      for rec in self:
+        rec.state = 'sold'
